90be539f68942297af6a030ec8ae5dccd418108f
Avish Jain <avishjain0@gmail.com>
2022-04-09 15:52:15 +0530

    login

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Contests from "./components/Contests/Contests";
+import Login from "./components/Login/Login";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/contests" element={<Contests />}></Route>
+        <Route path="/login" element={<Login />}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
